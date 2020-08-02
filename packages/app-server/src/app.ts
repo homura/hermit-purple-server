625d772d0020e3874c47ae2a9f5321ce4697310c
homura <homura.dev@gmail.com>
2020-08-02 21:52:29 +0800

    refactor: refactor with hermit-purple

--- packages/app-server/src/app.ts
+++ packages/app-server/src/app.ts
@@ -1,21 +1,18 @@
-import { envNum } from '@muta-extra/common';
-import { DefaultService } from '@muta-extra/knex-mysql';
-import { makeSchema, schemas as types } from '@muta-extra/nexus-schema';
+import { envNum, extendService, makeSchema } from '@muta-extra/hermit-purple';
 import { ApolloServer } from 'apollo-server';
 import path from 'path';
 
 const schema = makeSchema({
-  types,
+  // default types are bind on makeSchema
+  types: {},
   outputs: {
     schema: path.join(__dirname, '../api.graphql'),
   },
 });
 
-const services = new DefaultService();
-
 const server = new ApolloServer({
   schema,
-  context: { ...services },
+  context: extendService({}),
 });
 
 const port = envNum('HERMIT_PORT', 4040);
--- packages/app-sync/src/index.ts
+++ packages/app-sync/src/index.ts
@@ -1,31 +1,5 @@
 #!/usr/bin/env node
-require('@muta-extra/common').loadEnvFile();
+require('@muta-extra/hermit-purple').loadEnvFile();
 
-import {
-  DefaultLocalFetcher,
-  DefaultSyncEventHandler,
-} from '@muta-extra/knex-mysql';
-import {
-  DefaultRemoteFetcher,
-  IFetchLocalAdapter,
-  IFetchRemoteAdapter,
-  ISyncEventHandlerAdapter,
-  ISynchronizerAdapter,
-  PollingSynchronizer,
-} from '@muta-extra/synchronizer';
-
-const remoteFetcher: IFetchRemoteAdapter = new DefaultRemoteFetcher();
-const localFetcher: IFetchLocalAdapter = new DefaultLocalFetcher();
-const eventHandler: ISyncEventHandlerAdapter = new DefaultSyncEventHandler();
-
-const syncAdapter: ISynchronizerAdapter = {
-  ...remoteFetcher,
-  ...localFetcher,
-  ...eventHandler,
-};
-
-export function sync() {
-  return new PollingSynchronizer(syncAdapter).run();
-}
-
-sync();
+import { createSynchronizer } from '@muta-extra/hermit-purple';
+createSynchronizer().run();
