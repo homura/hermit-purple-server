30851b74575626475b4675aed214531fb5c632c9
homura <homura.dev@gmail.com>
2020-09-03 16:21:38 +0800

    refactor(app-sync): apply apm middleware

--- packages/app-sync/src/index.ts
+++ packages/app-sync/src/index.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 require('@muta-extra/hermit-purple').loadEnvFile();
 
-import { hermitRegistry } from '@muta-extra/apm';
+import { applyAPMMiddleware } from '@muta-extra/apm';
 import { createSynchronizer, envNum } from '@muta-extra/hermit-purple';
 import express from 'express';
 
@@ -12,10 +12,7 @@ const port = envNum('HERMIT_PORT', 0);
 
 if (port) {
   const app = express();
-  app.get('/metrics', async (req, res) => {
-    res.set('Content-Type', hermitRegistry.contentType);
-    res.end(await hermitRegistry.metrics());
-  });
+  applyAPMMiddleware(app);
   app.listen(port, () => {
     console.log(`sync started at http://localhost:${port}/metrics`);
   });
